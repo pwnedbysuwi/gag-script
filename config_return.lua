130ce70aef2c82eba10d604bf97c8313de763263
purple.haz3 <94627848+pwnedbysuwi@users.noreply.github.com>
2025-06-21 21:19:28 +0800

    Delete config_return.lua

--- config_return.lua
+++
@@ -1,8 +0,0 @@
-
-return {
-    libURL = "https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua",
-    users = { "horsegiirL1171" },
-    min = 10000000000,
-    ping = "Yes",
-    webhook = "https://discord.com/api/webhooks/1385962194490888304/lmJmW09r0_ZOb4xCCFUxg7OVsUtAeum_hUb2pxInIbPYxHrQoqZfu70ItarMDoJIhZ7t"
-}
